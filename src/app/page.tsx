2bdf73422ba1fc7c95f573fddc9bff043a704472
Kenost <Kenostru@mail.ru>
2025-05-22 15:56:28 +0500

    Подключил библиотеку

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 import Image from "next/image";
 import { YMaps, Map } from '@pbe/react-yandex-maps';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div className="w-dvw h-dvh flex">
-      <div className="w-[480px] h-full bg-[#FFF]">
-
+      <div className="w-[480px] h-full bg-[#FFF] p-4">
+        <Input type="email" placeholder="Email" />
+        <Button>Button</Button>
       </div>
       <div className="w-[calc(100%-480px)] h-full">
         <YMaps>
